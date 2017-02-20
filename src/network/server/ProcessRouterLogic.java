6b6c22fc1064265f4ec5b186c776d9ebb2202d97
SKCC16N01376\Administrator <zzzz1zzzz1@naver.com>
2017-02-20 21:29:32 +0900

    [2/20 9:29](방주선) RouterLogic 수정.

--- src/network/server/ProcessRouterLogic.java
+++ src/network/server/ProcessRouterLogic.java
@@ -1,19 +1,49 @@
 package network.server;
 
+import java.net.Socket;
+
+import db.domain.FileInfo;
 import db.domain.ResponseInfo;
 
-public class ProcessRouterLogic implements ProcessRouter {
+public class ProcessRouterLogic extends Thread implements ProcessRouter {
 	
+	private String userId;
+	private ServiceNum serviceNum;
+	private FileInfo fileInfo;
+	private Socket sock;
+	
+	public ProcessRouterLogic (Socket sock){
+		this.sock =sock;
+		
+	}
 	
 	/***
 	 * 요청번호를 받아서 ServiceNum(열거형)을 리턴함
 	 */
 	@Override
-	public ServiceNum depacketizer() {
+	public void depacketizer() {
 		// TODO Auto-generated method stub
-		return null;
+		this.userId=requestInfo.userId;
+		this.serviceNum=requestInfo.serviceNum;
+		//this.
 	}
 
+	/***
+	 * 
+	 */
+	@Override
+	public void runService(ServiceNum serviceNum) {
+		// TODO Auto-generated method stub
+		while(true){
+			
+			//RequestInfo requestInfo =(RequestInfo) read();
+			this.depacketizer();
+			if(this.serviceNum == ServiceNum.UPLOAD);
+			//하면 upload함수 호출
+		}
+	}
+	
+	
 	/***
 	 * FileInfo객체를 속성으로 갖고있는 ResponseInfo를 리턴함.
 	 * FileInfo에는 UserId와 FilePath갖고 있다.
@@ -24,14 +54,7 @@ public class ProcessRouterLogic implements ProcessRouter {
 		return null;
 	}
 
-	/***
-	 * 
-	 */
-	@Override
-	public void runService(ServiceNum serviceNum) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 	
 	
