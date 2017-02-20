e7d2f1cfe7d7d6877f743071acaaf05ddfc41648
SeungboKang <gogetit88@gmail.com>
2017-02-20 19:57:15 +0900

    [2/20 19:56](강승보, 방주선)인터페이스 구현체(TCPReactorLogic, ProcessRoutingLogic) 프로토타입 작성

---
+++ src/network/server/PorcessRouterLogic.java
@@ -0,0 +1,27 @@
+package network.server;
+
+import db.domain.ResponseInfo;
+
+public class PorcessRouterLogic implements PorcessRouter {
+
+	@Override
+	public ServiceNum depacketizer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseInfo packetizer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void runService(ServiceNum serviceNum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
---
+++ src/network/server/TCPReactorLogic.java
@@ -0,0 +1,21 @@
+package network.server;
+
+import java.net.Socket;
+
+import db.domain.RequestInfo;
+
+public class TCPReactorLogic implements TCPReactor {
+
+	@Override
+	public Socket getClient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void respondTo(RequestInfo requestInfo) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
