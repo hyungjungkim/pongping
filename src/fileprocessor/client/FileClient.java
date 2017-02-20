22511e5aa53aaf23abbb3886852c4e36c1141066
hyungjungkim <gnl4277@gmail.com>
2017-02-20 21:45:30 +0900

    test

--- src/fileprocessor/client/FileClient.java
+++ src/fileprocessor/client/FileClient.java
@@ -3,8 +3,6 @@ package fileprocessor.client;
 import java.io.IOException;
 import java.util.List;
 
-import db.domain.DirFile;
-
 public interface FileClient {
 	//
 	/*
@@ -13,17 +11,25 @@ public interface FileClient {
 	 *  currentPath => 현재 갖고있는 경로
 	 *  File Method
 	 */
+<<<<<<< HEAD
 	public List<DirFile> FileUpload(String userId, String localPath) throws IOException;
 	public boolean FileDownload(String userId, String localPath);
 	public List<DirFile> FileRemove(String userId, String currentPath);
 	public List<DirFile> ChangeName(String userId, String currentPath, String newPath);
 	public List<DirFile> FileSearch(String userId, String searchName);
+=======
+	public List<Object> FileUpload(String userId, String localPath);
+	public boolean FileDownload(String userId, String localPath);
+	public List<Object> FileRemove(String userId, String currentPath);
+	public boolean changeName(String userId, String currentPath);
+	public List<Object> FileSearch(String userId, String searchName);
+>>>>>>> e136e109014c30b6693426bf663d33ce4d4a032a
 	/*
 	 *  Directory Method
 	 */
-	public List<DirFile> DirectoryCreate(String userId, String currentPath);
-	public List<DirFile> DirectoryRemove(String userId, String currentPath);
-	public List<DirFile> ShowList(String userId, String currentPath);
+	public List<Object> DirectoryCreate(String userId, String currentPath);
+	public List<Object> DirectoryRemove(String userId, String currentPath);
+	public List<Object> ShowList(String userId, String currentPath);
 	
 	
 }
--- src/fileprocessor/server/FileSever.java
+++ src/fileprocessor/server/FileSever.java
@@ -3,23 +3,20 @@ package fileprocessor.server;
 import java.net.Socket;
 import java.util.List;
 
-import db.domain.DirFile;
-import db.domain.FileInfo;
-
 public interface FileSever {
 	//
 	/*
 	 * 
 	 * File Method
 	 */
-	public List<DirFile> FileUpload(Socket socket, FileInfo fileInfor);
-	public boolean FileDownload(Socket socket, FileInfo fileInfor);
-	public List<DirFile> FileRemove(String userId, String currentPath);
-	public List<DirFile> FileSearch(String userId, String searchName);
+	public List<Object> FileUpload(Socket socket, FileInfor fileInfor);
+	public boolean FileDownload(Socket socket, FileInfor fileInfor);
+	public List<Object> FileRemove(String userId, String currentPath);
+	public List<Object> FileSearch(String userId, String searchName);
 	/*
 	 *  Directory Method
 	 */
-	public List<DirFile> DirectoryCreate(Socket socket, FileInfo fileInfor);
-	public List<DirFile> DirectoryRemove(Socket socket, FileInfo fileInfor);
-	public List<DirFile> ShowList(String userId, String currentPath);
+	public List<Object> DirectoryCreate(Socket socket, FileInfor fileInfor);
+	public List<Object> DirectoryRemove(Socket socket, FileInfor fileInfor);
+	public List<Object> ShowList(String userId, String currentPath);
 }
