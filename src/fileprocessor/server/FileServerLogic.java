50cd9044aa8779c8eebe682d26f3d57bc4243397
SKCC16N01374\Administrator <jongwon5185@naver.com>
2017-02-21 14:05:37 +0900

    [2/21 14:05] (박정원) fileserverlogic과 인터페이스 수정

--- src/fileprocessor/server/FileServerLogic.java
+++ src/fileprocessor/server/FileServerLogic.java
@@ -2,14 +2,18 @@ package fileprocessor.server;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.List;
 
 import db.domain.DirFile;
 import db.domain.FileInfo;
+import db.domain.ListInfor;
 
 public class FileServerLogic implements FileSever {
 	private Socket sock;
@@ -17,20 +21,25 @@ public class FileServerLogic implements FileSever {
 	DataInputStream dis = null;
 	FileInputStream fis = null;
 	FileOutputStream fos = null;
-	
+	ObjectOutputStream out = null;
+	ObjectInputStream ois = null;
+
 	public FileServerLogic(Socket sock) {
 		// TODO Auto-generated constructor stub
 		this.sock = sock;
 	}
-	
+
 	@Override
-	public List<DirFile> FileUpload(FileInfo fileInfor) {
-		// TODO Auto-generated method stub
+	public List<DirFile> FileUpload(FileInfo fileInfor) throws IOException {
+		// TODO client requesting path => to DB
+		String serverSavePath = null; // from DB
+		out = new ObjectOutputStream(sock.getOutputStream());
+		ois = new ObjectInputStream(sock.getInputStream()); 
 		byte[] contentBytes = new byte[1024];
 
 		try {
 			dis = new DataInputStream(sock.getInputStream());
-			fos = new FileOutputStream(fileInfor.getFilePath());
+			fos = new FileOutputStream(serverSavePath);
 			while (true) {
 				int count = dis.read(contentBytes);
 				if (count == -1) {
@@ -41,18 +50,53 @@ public class FileServerLogic implements FileSever {
 		} catch (IOException e) {
 			e.getStackTrace();
 		}
+		// TODO current list of current depth (from DB)
+		// Serializable
+		ListInfor retList = new ListInfor();
+		// TODO retList.setListInfor(list);
+		out.writeObject(retList);
 		return null;
 	}
 
 	@Override
-	public boolean FileDownload(FileInfo fileInfor) {
-		// TODO Auto-generated method stub
-		return false;
+	public void FileDownload(FileInfo fileInfor) throws IOException {
+		// TODO client requesting path => to DB
+		String serverDownPath = null; // from DB
+		byte[] contentBytes = new byte[1024];
+
+		try {
+			dos = new DataOutputStream(sock.getOutputStream());
+			fis = new FileInputStream(serverDownPath);
+			while (true) {
+				int count = fis.read(contentBytes);
+				if (count == -1) {
+					break;
+				}
+
+				dos.write(contentBytes, 0, count);
+			}
+		} catch (IOException e) {
+			e.getStackTrace();
+		}
+		// TODO
 	}
 
 	@Override
-	public List<DirFile> FileRemove(String userId, String currentPath) {
-		// TODO Auto-generated method stub
+	public List<DirFile> FileRemove(String userId, String currentPath) throws IOException {
+		// TODO client requesting path => to DB
+		String fileRemovePath = null; // from DB
+		ois = new ObjectInputStream(sock.getInputStream()); 
+		File file = new File(fileRemovePath);
+		if(file.delete()){
+			System.out.println(file.getName() + " is deleted!");
+		}else{
+			System.out.println("Delete operation is failed.");
+		}
+		// TODO current list of current depth (from DB)
+		// Serializable
+		ListInfor retList = new ListInfor();
+		// TODO retList.setListInfor(list);
+		out.writeObject(retList);
 		return null;
 	}
 
@@ -82,9 +126,9 @@ public class FileServerLogic implements FileSever {
 
 	@Override
 	public List<DirFile> ChangeName(String userId, String currentPath, String newPath) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
+		// 회의 필요
 		return null;
 	}
-
 
 }
--- src/fileprocessor/server/FileSever.java
+++ src/fileprocessor/server/FileSever.java
@@ -1,5 +1,6 @@
 package fileprocessor.server;
 
+import java.io.IOException;
 import java.net.Socket;
 import java.util.List;
 
@@ -12,9 +13,9 @@ public interface FileSever {
 	 * 
 	 * File Method
 	 */
-	public List<DirFile> FileUpload(FileInfo fileInfor);
-	public boolean FileDownload(FileInfo fileInfor);
-	public List<DirFile> FileRemove(String userId, String currentPath);
+	public List<DirFile> FileUpload(FileInfo fileInfor) throws IOException;
+	public void FileDownload(FileInfo fileInfor) throws IOException;
+	public List<DirFile> FileRemove(String userId, String currentPath) throws IOException;
 	public List<DirFile> ChangeName(String userId, String currentPath, String newPath);
 	public List<DirFile> FileSearch(String userId, String searchName);
 	/*
