8cb027e0203d1d9b7ce6c780d80d61eadb1efd13
SeungboKang <gogetit88@gmail.com>
2017-02-21 09:17:58 +0900

    [2/21 09:17](강승보)ProcessRouter 수정(계속)

--- src/network/server/ProcessRouter.java
+++ src/network/server/ProcessRouter.java
@@ -1,11 +1,7 @@
 package network.server;
 
-import db.domain.ResponseInfo;
-
 public interface ProcessRouter {
 	
 	public void depacketizer();
-	public ResponseInfo packetizer();
-	public void runService(ServiceNum serviceNum);
 		
 }
--- src/network/server/ProcessRouterLogic.java
+++ src/network/server/ProcessRouterLogic.java
@@ -12,7 +12,7 @@ public class ProcessRouterLogic extends Thread implements ProcessRouter {
 	private FileInfo fileInfo;
 	private Socket sock;
 	
-	public ProcessRouterLogic (Socket sock){
+	public ProcessRouterLogic(Socket sock){
 		this.sock =sock;
 		
 	}
@@ -31,9 +31,10 @@ public class ProcessRouterLogic extends Thread implements ProcessRouter {
 	/***
 	 * 
 	 */
-	@Override
-	public void runService(ServiceNum serviceNum) {
+	
+	public void run() {
 		// TODO Auto-generated method stub
+		super.run();
 		while(true){
 			
 			//RequestInfo requestInfo =(RequestInfo) read();
@@ -41,9 +42,9 @@ public class ProcessRouterLogic extends Thread implements ProcessRouter {
 			if(this.serviceNum == ServiceNum.UPLOAD);
 			//하면 upload함수 호출
 		}
+		
 	}
-	
-	
+
 	/***
 	 * FileInfo객체를 속성으로 갖고있는 ResponseInfo를 리턴함.
 	 * FileInfo에는 UserId와 FilePath갖고 있다.
