a8f54f7e6fbec1132f1c77713bb6aed92fb3e3ac
SeungboKang <gogetit88@gmail.com>
2017-02-22 13:14:17 +0900

    [2/22 13:14](강승보, 방주선) HandlerPoolManager 추가, ThreadManager 수정

--- src/network/server/ThreadManager.java
+++ src/network/server/ThreadManager.java
@@ -1,6 +1,95 @@
 package network.server;
 
+import java.util.List;
+
+import fileprocessor.server.ChangeNameRunnable;
+import fileprocessor.server.DirectoryCreateRunnable;
+
 public class ThreadManager {
-//
+	//
+	private List<Thread> changeNameRunnables;
+	private List<Thread> directoryCreateRunnables;
+	private List<Thread> directoryRemoveRunnables;
+	private List<Thread> fileDownloadRunnables;
+	private List<Thread> fileRemoveRunnables;
+	private List<Thread> fileSearchRunnables;
+	private List<Thread> fileUploadRunnables;
+	private List<Thread> showListRunnables;
+	
+	
+	
+	public ThreadManager(){
+		
+	}
+	
+	public void runThread(){
+		
+		Thread handlerThread; 
+				
+		for(int i = 0; i < 3; i++){
+			
+			ChangeNameRunnable changeNameRunnable = new ChangeNameRunnable();
+			handlerThread = new Thread(changeNameRunnable);
+			this.changeNameRunnables.add(handlerThread);
+			
+		}
+		
+		for(int i = 0; i < 3; i++){
+			
+			DirectoryCreateRunnable directoryCreateRunnable = new DirectoryCreateRunnable();
+			handlerThread = new Thread(directoryCreateRunnable);
+			this.changeNameRunnables.add(handlerThread);
+			
+		}
+		
+		for(int i = 0; i < 3; i++){
+			
+			ChangeNameRunnable changeNameRunnable = new ChangeNameRunnable();
+			handlerThread = new Thread(changeNameRunnable);
+			this.changeNameRunnables.add(handlerThread);
+			
+		}
+		
+		for(int i = 0; i < 3; i++){
+			
+			ChangeNameRunnable changeNameRunnable = new ChangeNameRunnable();
+			handlerThread = new Thread(changeNameRunnable);
+			this.changeNameRunnables.add(handlerThread);
+			
+		}
+		
+		for(int i = 0; i < 3; i++){
+			
+			ChangeNameRunnable changeNameRunnable = new ChangeNameRunnable();
+			handlerThread = new Thread(changeNameRunnable);
+			this.changeNameRunnables.add(handlerThread);
+			
+		}
+		
+		for(int i = 0; i < 3; i++){
+			
+			ChangeNameRunnable changeNameRunnable = new ChangeNameRunnable();
+			handlerThread = new Thread(changeNameRunnable);
+			this.changeNameRunnables.add(handlerThread);
+			
+		}
+		
+		for(int i = 0; i < 3; i++){
+			
+			ChangeNameRunnable changeNameRunnable = new ChangeNameRunnable();
+			handlerThread = new Thread(changeNameRunnable);
+			this.changeNameRunnables.add(handlerThread);
+			
+		}
+		
+		for(int i = 0; i < 3; i++){
+			
+			ChangeNameRunnable changeNameRunnable = new ChangeNameRunnable();
+			handlerThread = new Thread(changeNameRunnable);
+			this.changeNameRunnables.add(handlerThread);
+			
+		}
+		
+	}
 	
 }
