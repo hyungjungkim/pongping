11f80485a0d5db46c24da02f80873f5749a145d1
SKCC16N01374\Administrator <jongwon5185@naver.com>
2017-02-22 11:47:02 +0900

    [2.22 11:46] (박정원) fileserver fileremove 수정

--- src/fileprocessor/server/FileRemoveRunnable.java
+++ src/fileprocessor/server/FileRemoveRunnable.java
@@ -9,31 +9,32 @@ import java.util.List;
 import db.domain.DirFile;
 import db.domain.FileInfo;
 import db.domain.ListInfor;
+import db.store.DBStore;
 
 public class FileRemoveRunnable implements Runnable{
 	//
 	private FileInfo fileInfo = null;
 	private Socket sock = null;
 	private ObjectOutputStream out = null;
-	
+	private DBStore dbStore;
 	
 	public FileRemoveRunnable(FileInfo fileInfo, Socket sock){
 		this.fileInfo = fileInfo;
 		this.sock = sock;
+		this.dbStore = DBStore.getInstance(fileInfo.getUserId());
 	}
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
 		try {
 			this.FileRemove(this.fileInfo.getUserId(), this.fileInfo.getCurrentPath());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	public List<DirFile> FileRemove(String userId, String currentPath) throws IOException {
-		// TODO client requesting path => to DB
-		String fileRemovePath = null; // from DB
+		// client requesting path => to DB
+		String clientPath = currentPath;
+		String fileRemovePath = dbStore.FileRemove(clientPath); // from DB
 		out = new ObjectOutputStream(sock.getOutputStream());
 		File file = new File(fileRemovePath);
 		if(file.delete()){
@@ -41,11 +42,18 @@ public class FileRemoveRunnable implements Runnable{
 		}else{
 			System.out.println("Delete operation is failed.");
 		}
-		// TODO current list of current depth (from DB)
+		// current list of current depth (from DB)
+		String parentPath = clientPath.substring(0, clientPath.lastIndexOf("/"));
 		// Serializable
-		ListInfor retList = new ListInfor();
-		// TODO retList.setListInfor(list);
-		out.writeObject(retList);
+				try {
+					ListInfor retList = new ListInfor();
+					retList.setListInfor(dbStore.ShowList(parentPath));
+					out.writeObject(retList);
+				} catch (IOException e) {
+					e.getStackTrace();
+				} finally {
+					out.close();
+				}
 		return null;
 	}
 }
